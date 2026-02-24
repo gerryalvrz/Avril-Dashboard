4c7549b454d4fa03be4372d43db3b843bbb356f8
AgentMotus <agentmotus@users.noreply.github.com>
2026-02-24 04:53:06 -0500

    fix(build): force @base-org/account browser entries for waap in next webpack

--- next.config.mjs
+++ next.config.mjs
@@ -1,2 +1,17 @@
-const nextConfig = { reactStrictMode: true };
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve = config.resolve || {};
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      '@base-org/account/dist/index.node.js': '@base-org/account/dist/index.js',
+      '@base-org/account/dist/interface/payment/index.node.js':
+        '@base-org/account/dist/interface/payment/index.js',
+      '@base-org/account/node': '@base-org/account/browser',
+    };
+
+    return config;
+  },
+};
+
 export default nextConfig;
